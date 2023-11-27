3e28d2f40bd8f6979f1ec519e71c01216915b1fd
djsamex@yahoo.com <djsamex@yahoo.com>
2023-11-27 04:57:42 -0500

    added mwa feature: injury recovery module

---
+++ src/domains/d05mwa/InjuryRecovModule.mjs
@@ -0,0 +1,126 @@
+
+/*
+
+Contains Injury Recovery Module... 
+
+Testing steps:
+1. To avoid hardcoding these parameters, set your environment variables before launching.
+2. You also need to register your application at https://login.inrupt.com/registration.html
+
+export CLIENTID="XXXX"
+export CLIENTSECRET="XXXX"
+export CLIENTNAME="Postman"  --- Change this  you are using a different application..
+export REDIRECTURL="http://localhost:8080/callback" -- Change your port if necessary
+export OIDCISSUER="https://login.inrupt.com"
+export WEBID="XXX" --- Your webId
+export LISTENPORT=8080 -- Change your port if necessary
+
+Then invoke your file:
+
+    node stressHistorySvr.mjs
+
+*/
+
+import {
+    getSolidDataset,
+    createSolidDataset,
+    setThing,
+    saveSolidDatasetAt,
+    createThing,
+    getPodUrlAll,
+    setStringNoLocale,
+    getThingAll,
+    removeThing
+} from '@inrupt/solid-client';
+
+import bodyParser from 'body-parser';
+import express from 'express';
+import cors from 'cors';
+
+// const session = getSessionFromStorage("mainSession");
+import { Session } from "@inrupt/solid-client-authn-node";
+import { fetch } from "@inrupt/solid-client-authn-browser";
+
+import { SCHEMA_INRUPT, RDF, AS } from "@inrupt/vocab-common-rdf";
+
+/* Open new session ... */
+const session = new Session();
+async function ensureLoggedIn(req, res, next) {
+    try {
+
+        if (session && !session.info.isLoggedIn) {
+            const authHeader = req.headers.authorization;
+            if (authHeader && authHeader.startsWith('Bearer ')) {
+                const token = authHeader.split(' ')[1];
+                //console.log("Token -----------" + token)
+                try {
+                    await session.login({
+                        clientName: process.env.CLIENTNAME,
+                        oidcIssuer: process.env.OIDCISSUER,
+                        redirectUrl: process.env.REDIRECTURL,
+                        clientId: process.env.CLIENTID,
+                        clientSecret: process.env.CLIENTSECRET,
+                        token: token
+                    });
+                    //console.log("Successfully logged in -----------")
+                    next();
+                } catch (error) {
+                    res.status(401).send("Authentication failed.");
+                    console.log("Authentication failed. === " + error)
+                }
+            } else {
+                res.status(401).send("Bearer token missing.");
+                console.log("Bearer token missing. ")
+            }
+        } else {
+            next();
+        }
+
+    } catch (error2) {
+        console.log("error === " + error2)
+    }
+}
+
+//console.log("Server.js is starting..." + session);
+async function getDefaultPod() {
+    console.log("starting getDefaultPod execution...");
+    const mypods = await getPodUrlAll(process.env.WEBID, { fetch: fetch });
+    return mypods[0];
+};
+
+ 
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(ensureLoggedIn);
+
+app.get('/callback', async (req, res) => {
+    try {
+        await session.handleIncomingRedirect(req.url);
+        res.send("Authentication successful.");
+    } catch (error) {
+        res.status(500).send("Error handling authentication callback.");
+    }
+});
+
+app.listen(process.env.LISTENPORT, () => {
+    console.log(`Server started on port ${process.env.LISTENPORT}....`);
+});
+
+
+/* helper_util functions for conversion */
+function toInt(value) {
+    return value ? parseInt(value) : null;
+}
+
+function toFloat(value) {
+    return value ? parseFloat(value) : null;
+}
+
+// some class vars
+const parent_path = 'wellness/mental_wellness_athelete/injury_recov'
+
+
+
+ 
