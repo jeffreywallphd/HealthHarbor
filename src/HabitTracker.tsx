5842fe564127d958f43d0c68d3773556beb85e55
huntermali <hemalino@mtu.edu>
2024-03-25 00:31:06 -0400

    Side Nav Bar
    Code from Ganesh I was given, refactored for React (JavaScript not yet implemented)

--- src/HabitTracker.tsx
+++ src/HabitTracker.tsx
@@ -1,28 +1,26 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
+import "./front/HabitTracker/styles.css";
 
 class HabitTracker extends Component {
   render() {
     return (
-      <div>
-        <h2>Habit Tracker</h2>
-        <p>Track habits and converse with an AI for mental health support.</p>
-        <section className="cardRow">
-          <NavLink to="/habit-tracker">
-            <div className="card">
-              <div className="cardHeader">
-                <h3>Habit Tracker</h3>
-              </div>
-              <div className="cardContainer">
-                <p>
-                  Monitor and maintain your daily habits for better mental
-                  health.
-                </p>
-              </div>
-            </div>
-          </NavLink>
-        </section>
-      </div>
+        <div className="sidenav" id="sidenav">
+          <img
+              src="https://play-lh.googleusercontent.com/ODq6rmF4jOnXCzF0OH8BujnoDTzCtQgFLaNwiqjoVIg5t0Wpy9jkS7yY6AmgrYs98No"
+              alt="Icon Image"></img>
+          <a href="main.html" id="habitBtn">Habit</a>
+          <a href="#" id="viewOptionsBtn">View Options</a>
+          <div id="viewOptions">
+            <a href="#">Daily</a>
+            <a href="Calendar.html">Weekly</a>
+          </div>
+          <div className="gap"></div>
+
+          <div className="gap"></div>
+          <a href="#" id="dueBtn">Due</a>
+          <a href="#" id="habitReportBtn">Habit Report</a>
+        </div>
     );
   }
 }
