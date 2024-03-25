f04463067f35b20be5929b646420e5b9eb726617
huntermali <hemalino@mtu.edu>
2024-03-25 11:07:04 -0400

    Update CSS for sidenav
    - Added new habittracker.css file
    - Edited CSS to be more in line with UI guidelines

--- src/View/HabitTracker.tsx
+++ src/View/HabitTracker.tsx
@@ -1,14 +1,11 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
-import "./front/HabitTracker/styles.css";
+import "../styles/habittracker.css";
 
 class HabitTracker extends Component {
   render() {
     return (
         <div className="sidenav" id="sidenav">
-          <img
-              src="https://play-lh.googleusercontent.com/ODq6rmF4jOnXCzF0OH8BujnoDTzCtQgFLaNwiqjoVIg5t0Wpy9jkS7yY6AmgrYs98No"
-              alt="Icon Image"></img>
           <a href="main.html" id="habitBtn">Habit</a>
           <a href="#" id="viewOptionsBtn">View Options</a>
           <div id="viewOptions">
