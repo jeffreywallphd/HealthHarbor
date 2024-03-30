c00a14a4dc24c9da690b280d85b71028c1f34ca0
Divy Patel <divypatel125710@gmail.com>
2024-03-30 03:18:20 -0400

    MHCB Chatbot UI file refacored code

--- src/View/Mental_chatbot.tsx
+++ src/View/Mental_chatbot.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { generateText } from "../Controller/prototype.js";
+import './Mental_chatbot.css'
 
 interface Message {
   message: string;
@@ -60,33 +61,16 @@ class MentalHealthChatBot extends Component<{}, State> {
     
     return (
       
-      <div className="chat-container" style={{ display: 'flex', flexDirection: 'column', 
-      justifyContent: 'center', alignItems: 'center', backgroundColor: '#555654', 
-      width: '100%', height: '450px', margin: 'auto', padding: '2px'}}>
-          <div className="chat-header"style={{marginLeft:'100px',justifyContent: 'center', alignItems: 'center',marginTop:'2px',
-        position: 'sticky',top:1 /* Changed to sticky */}}>
-          
-    <h1 style={{marginRight:'100px',justifyContent: 'center', 
-    alignItems: 'center',color:'white',
-    }}>Mental Health ChatBot</h1>
+      <div className="chat-container">
+          <div className="chat-header">      
+    <h1>Mental Health ChatBot</h1>
     </div>
-      <div className='buttons-container'
-      style={{ display: 'flex', flexDirection: 'column',
-       position: 'absolute', left: '20px', top: '300px', 
-       backgroundColor: '#868885', width: '350px', 
-       height: '455px', alignItems: 'flex-start',
-       paddingLeft:'10px' }}>
-            <button className="recent-button" onClick={() => alert('Viewing recent conversations...')}
-            style={{backgroundColor:'#555654',color:'white'}}>
+      <div className='buttons-container'>
+            <button className="recent-button" onClick={() => alert('Viewing recent conversations...')}>
               Recent
             </button>
-            
-          </div>
-
-          
-        <div className="chat-messages" style={{ flex: 1, marginBottom: '10px', color: 'white',
-         paddingRight: '10px', scrollbarWidth:'auto', overflowY: 'scroll',width:'700px',
-          scrollbarColor: '#888 #f1f1f1',paddingLeft:'100px', direction:'ltr'}}>
+      </div>    
+        <div className="chat-messages">
         <p> Hello! </p>
           {messages.map((msg, index) => (
             <div key={index} className={`chat-message ${msg.sender}-message`}>
@@ -102,45 +86,19 @@ class MentalHealthChatBot extends Component<{}, State> {
           )}
         
 
-        <div className="chat-input-container"
-         style={{
-          display: 'flex',
-          alignItems: 'center', 
-          marginBottom: '20px',
-          paddingTop:'200px',
-         }}>
+        <div className="chat-input-container">
           <input
             type="text"
             className="chat-input"
             placeholder="Type your message..."
             value={userInput}
             onChange={(e) => this.setState({ userInput: e.target.value })}
-            onKeyPress={this.handleKeyPress} 
-            style={{
-              flex: 1,
-              width: '500px', 
-              height: '40px', 
-              marginRight: '10px', 
-              padding: '8px',
-              border: '1px solid #868885',
-              
-            }}
-          />
+            onKeyPress={this.handleKeyPress}/>
           <head>
         {/* send Icons font link */}
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
       </head>
-          <button className="send-button" onClick={this.sendMessage}
-              style={{
-                padding: '8px',
-                borderRadius: '50%',
-                backgroundColor: '#868885',
-                color: '#fff',
-                border: 'none',
-                cursor: 'pointer',
-                position:'sticky',
-                bottom:10,
-              }}>
+          <button className="send-button" onClick={this.sendMessage}>
           <i className="material-icons">send</i>
       
           </button>
